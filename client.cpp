89a38bb865213f7e5c443e8ed6fec1a095b3d931
Hector Idme <felipeidme04@gmail.com>
2021-04-13 01:38:29 -0500

    protocolo - añadido(login,lista,salir)

--- client.cpp
+++ client.cpp
@@ -1,5 +1,5 @@
 /* Client code in C++ */
-
+//g++ client.cpp -o client.exe -lpthread
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,11 +10,169 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <vector>
 #include <thread>
 
 using namespace std;
 int SocketFD;
+string comando;
+bool salida = false;
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+int getSize(int s){
+    string tam = "9";
+    int res;
+    for(int i=1;i<s;i++){tam += "9";}
+    res = stoi(tam);
+    return res;
+}
+
+int len(char* w){
+    int count =0;
+    while(*w != '\0'){
+        count++;
+        w++;
+    }
+    return count;
+}
+
+string formatNumbers(int range,int number){
+    string numb = to_string(number);
+    if(numb.size() < range){
+        for(int i=1;i<range;i++){
+            numb.insert(0,"0");
+        }
+    }
+    return numb;
+}
+
+
+struct login{
+  char accion;
+  char tamanio_user[2];
+  char tamanio_password[2];
+  char* user;
+  char* password;
+
+  login(){
+    size_t tam_user = sizeof(tamanio_user)/sizeof(tamanio_user[0]);
+    size_t tam_pass = sizeof(tamanio_password)/sizeof(tamanio_password[0]);
+    accion = 'l';
+    user = new char[getSize(tam_user)];
+    password = new char[getSize(tam_pass)];
+  }
+
+  void loginView(){
+    cout<<"LOGIN"<<endl;
+    cout<<"Username: "; cin>>user;
+    cout<<"Password: "; cin>>password;
+  }
+
+  void print(){
+      cout<<user<<endl;
+      cout<<password<<endl;
+  }
+
+  string make_packet(){
+
+    size_t tam_user = sizeof(tamanio_user)/sizeof(tamanio_user[0]);
+    size_t tam_pass = sizeof(tamanio_password)/sizeof(tamanio_password[0]);
+    int numberChar_user = len(user);
+    int numberChar_pass = len(password);
+
+    string user_size = formatNumbers(tam_user,numberChar_user);
+    string pass_size = formatNumbers(tam_pass,numberChar_pass);
+
+    string packet = "";
+
+    packet += accion;
+    packet += user_size;
+    packet += pass_size;
+    packet += user;
+    packet += password;
+
+    return packet;
+  }
+
+};
+
+struct list{
+  char accion;
+
+  list(){
+    accion = 'i';
+  }
+
+  char make_packet(){
+    return accion;
+  }
+};
+
+
+struct salir{
+  char accion;
+
+  salir(){
+    accion = 'x';
+  }
+
+  char make_packet(){
+    return accion;
+  }
+};
+
+
+bool getDataFromPacket_Okey(string msg){
+  if(msg[0] != 'L'){return false;}
+  msg.erase(0,1);
+  cout<<msg<<endl;
+  return true;
+}
+
+bool getDataFromPacket_List(string msg){
+  if(msg[0] != 'I'){return false;}
+
+  vector<string> users;
+  vector<int> tamanios;
+  string u = "";
+  string tamU,numCli;
+  int tam_u,num_clients;
+
+  msg.erase(0,1);
+
+  numCli = msg.substr(0,2);
+  num_clients = stoi(numCli);
+
+  msg.erase(0,2);
+
+  for(int i=0;i<num_clients;i++){
+    tamU = msg.substr(0,2);
+    tam_u = stoi(tamU);
+
+    tamanios.push_back(tam_u);
+    msg.erase(0,2);
+  }
+
+  for(int i=0;i<num_clients;i++){
+    u = msg.substr(0,tamanios[i]);
+    users.push_back(u);
+    msg.erase(0,tamanios[i]);
+  }
+  
+  for(int i=0;i<users.size();i++){
+    cout<<"[-]"<<users[i]<<endl;
+  }
+
+  if(msg.size() == 0){return true;}
+  else{return false;}
+}
+
+
+
+////////////////////////////////////////////
+////////////////////////////////////////////
 
 void Connection ( string IP, int port)
 {
@@ -58,31 +216,77 @@ void Connection ( string IP, int port)
 
 void Listen()
 { 
-  while(1)
-  {
-    int n;
-    char buffer[256];
-    bzero(buffer,256);
-    n = read(SocketFD, buffer,256);
-    if (n < 0) perror("ERROR reading from socket");
-    cout<<buffer<<endl;
+  char b[256];
+  bzero(b,256);
+  read(SocketFD, b,256);
+  bool validate_login = getDataFromPacket_Okey(b);
+  cout<<"login: "<<validate_login<<endl;
+
+  if(validate_login){
+    while(1)
+    {
+      int n;
+      char buffer[256];
+      bzero(buffer,256);
+      n = read(SocketFD, buffer,256);
+      if (n < 0){
+        //perror("ERROR reading from socket");
+        break;
+      }
+      else if(comando == "i"){
+        getDataFromPacket_List(buffer);
+      }
+      else if(comando == "x"){
+        if(strcmp(buffer,"X")==0){
+          salida = true;
+        }
+      }
+      else{
+        cout<<buffer<<endl;
+      }
+    }
   }
 }
 
 
 int main(void)
 {
-  Connection("100.107.106.165",50001);
+  Connection("127.0.0.1",45000);
   int n;
+  
+
+  login u;
+  u.loginView();
+  string msg = u.make_packet();
+  write(SocketFD,msg.c_str(),msg.size());
+
   thread(Listen).detach();
+
   while( 1 )
   { 
+      if(salida){break;}
       string message;
-      //cout<<"Type a message: ";
+      cout<<">>";
       message="";
       getline(cin,message);
-      n = write(SocketFD,message.c_str(),message.length());      
+      comando = message;
+      
+      if(comando == "i"){
+        list l;
+        message = l.make_packet();
+      }
+      else if(comando == "x"){
+        salir e;
+        message = e.make_packet();
+      }
+      n = write(SocketFD,message.c_str(),message.length());
+      if (n < 0){
+        //perror("ERROR reading from socket");
+        break;
+      }    
   }
+
+  
 
   shutdown(SocketFD, SHUT_RDWR);
 
--- server.cpp
+++ server.cpp
@@ -1,5 +1,5 @@
   /* Server code in C */
- 
+ //g++ server.cpp -o server.exe -lpthread
   #include <sys/types.h>
   #include <sys/socket.h>
   #include <netinet/in.h>
@@ -9,24 +9,215 @@
   #include <stdlib.h>
   #include <string.h>
   #include <unistd.h>
+  #include <utility>
 
   #include <thread>
   #include <vector>
 
   using namespace std;
  
-vector<int> clients;
+//vector<int> clients;
+vector<pair<int,vector<string>>> clientes;
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+string getUser(int id){
+  string user;
+  for(int i=0;i<clientes.size();i++){
+    if(clientes[i].first == id){
+        user = clientes[i].second[0];
+        break;
+    }
+  }
+  return user;
+}
+
+
+int getSize(int s){
+    string tam = "9";
+    int res;
+    for(int i=1;i<s;i++){tam += "9";}
+    res = stoi(tam);
+    return res;
+}
+
+int len(char* w){
+    int count =0;
+    while(*w != '\0'){
+        count++;
+        w++;
+    }
+    return count;
+}
+
+string formatNumbers(int range,int number){
+    string numb = to_string(number);
+    if(numb.size() < range){
+        for(int i=1;i<range;i++){
+            numb.insert(0,"0");
+        }
+    }
+    return numb;
+}
+
+bool getDataFromPacket_Login(string msg,vector<string> &data){
+  string u,pssw;
+  string tamU_str,tamPSSW_str;
+  int tam_u,tam_pssw;
+
+  if(msg[0] != 'l'){return false;}
+
+  msg.erase(0,1);
+
+  tamU_str = msg.substr(0,2);
+  tam_u = stoi(tamU_str);
+  
+  msg.erase(0,2);
+
+  tamPSSW_str = msg.substr(0,2);
+  tam_pssw = stoi(tamPSSW_str);
+  
+  msg.erase(0,2);
+
+  u = msg.substr(0,tam_u);
+  msg.erase(0,tam_u);
+
+  pssw = msg.substr(0,tam_pssw);
+  msg.erase(0,tam_pssw);
+
+  data.push_back(u);
+  data.push_back(pssw);
+
+  if(msg.size() == 0){return true;}
+  else{return false;}
+
+}
+
+struct list{
+  char accion;
+  char num_users[2];
+  char tamanio_user_name[2];
+  char* user_name;
+
+  list(){
+    accion = 'I';
+  }
+
+  string make_packet(){
+    string packet = "";
+    int num_clientes = clientes.size();
+    size_t num_users_tam = sizeof(num_users)/sizeof(num_users[0]);
+    string cant_clients = formatNumbers(num_users_tam,num_clientes);
+
+    size_t tam_user_name;
+    string tam_user_name_format;
+    string user_names="";
+
+    packet += accion;
+    packet += cant_clients;
+
+    for(int i=0;i<num_clientes;i++){
+      tam_user_name = sizeof(tamanio_user_name)/sizeof(tamanio_user_name[0]);
+      user_names += clientes[i].second[0];
+      tam_user_name_format = formatNumbers(tam_user_name,clientes[i].second[0].size());
+      packet += tam_user_name_format;
+    }
+    packet += user_names;
+
+    return packet;
+  }
+};
+
+
+struct okey{
+  char accion;
+  char ok[3];
+
+  okey(){
+    accion = 'L';
+    strcpy(ok,"ok");
+  }
+
+  string make_packet(){
+    string packet = "";
+
+    packet += accion;
+    packet += ok;
+
+    return packet;
+  }
+};
+
+struct error{
+  char accion;
+  char error_msg[20];
+/*
+  void set_msg(string msg){
+    strcpy(error_msg,msg);
+  }  */
+
+  string make_packet(){
+    string packet = " ";
+    
+    packet = accion;
+    packet += error_msg;
+
+    return packet;
+  }
+};
+
+struct salir{
+  char accion;
+
+  salir(){
+    accion = 'X';
+  }
+
+  char make_packet(){
+    return accion;
+  }
+
+  void deleteSocket(int id){
+    int pos=0;
+    for(;pos<clientes.size();pos++){
+      if(clientes[pos].first == id){break;}
+    }
+
+    //shutdown(id, SHUT_RDWR);
+    //close(id);
+    clientes.erase(clientes.begin()+pos);
+  }
+};
+
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+
 
 void Broadcast(int ConnectFD, string message) 
 {
   //write(ConnectFD, message.c_str(),message.length());
-  
-  for (int i = 0; i < clients.size(); i++) {
-    write(clients[i], message.c_str(),message.length());
-  }
-  
+  /*
+  cout<<"Clients: ";
+  for(int i=0;i<clients.size();i++){
+    cout<<clients[i]<<",";
+  }cout<<endl;
 
+  cout<<"Clientes: ";
+  for(int i=0;i<clientes.size();i++){
+    cout<<"["<<clientes[i].first<<"-("<<clientes[i].second[0]<<"|"<<clientes[i].second[1]<<")] ,";
+  }cout<<endl;
+  */
+
+  for (int i = 0; i < clientes.size(); i++) {
+    if(clientes[i].first != ConnectFD){
+      write(clientes[i].first, message.c_str(),message.length());
+    }
+  }
 }
+
 
 void Connection( int &SocketFD , int port)
 {
@@ -68,27 +259,73 @@ void listenClient(int ConnectFD)
     int n;
 
     string message;
-    while(1)
-    {
-      bzero(buffer,256); 
-      n = read(ConnectFD,buffer,255);
-      if (n < 0) perror("ERROR reading from socket");
-      printf("Client %i: %s\n",ConnectFD,buffer);
-      //cout<<"Type a message: ";
-      message="Client " + to_string(ConnectFD) + ":" + buffer;
-      //getline(cin,message);
-      Broadcast(ConnectFD, message);
-    }
 
-    for(int i = 0; i < clients.size(); ++i) 
+    bzero(buffer,256); 
+    n = read(ConnectFD,buffer,255);
+    if (n < 0) perror("ERROR reading from socket");
+
+    vector<string> datos_login;
+    bool validate_login= getDataFromPacket_Login(buffer,datos_login);
+
+    if(validate_login){
+
+      auto par = make_pair(ConnectFD,datos_login);
+      clientes.push_back(par);
+      
+      okey k;
+      string msg = k.make_packet();
+      write(ConnectFD, msg.c_str(),msg.size());
+
+      while(1)
+      {
+
+        bzero(buffer,256); 
+        n = read(ConnectFD,buffer,255);
+        if (n < 0){
+           //perror("ERROR reading from socket");
+           break;
+        }
+         string user = getUser(ConnectFD);
+
+        if(strcmp(buffer, "i") == 0){
+          list users;
+          msg = users.make_packet();
+          write(ConnectFD,msg.c_str(),msg.size());
+        }
+
+        else if(strcmp(buffer, "x") == 0){
+          salir e;
+          msg = e.make_packet();
+          write(ConnectFD,msg.c_str(),msg.size());
+          e.deleteSocket(ConnectFD);
+          close(ConnectFD);
+          if(clientes.size()){
+            ConnectFD = clientes[0].first;
+          }
+        }
+
+        else{
+          //cout<<user<<": "<<buffer<<endl;
+          //cout<<"Type a message: ";
+          message= user + ": " + buffer;
+          //getline(cin,message);
+          Broadcast(ConnectFD, message);
+        }
+      }
+    }
+    
+    /*
+    cout<<"borrar server"<<endl;
+    for(int i = 0; i < clientes.size(); ++i) 
     {
-      if (clients[i] == ConnectFD) 
+      if (clientes[i].first == ConnectFD) 
       {
         shutdown(ConnectFD, SHUT_RDWR);
         close(ConnectFD);
-        clients.erase(clients.begin() + i);
+        clientes.erase(clientes.begin() + i);
       }
     }
+    */
 }
 
 int main(void)
@@ -97,14 +334,13 @@ int main(void)
   int SocketFD;
   char buffer[256];
   int n;
-  cout<<"Hi"<<endl;
+  cout<<"Beginning the server"<<endl;
   Connection( SocketFD, 45000);
   
   cout<<"Connection initialized"<<endl;
   
   while(1)
   {
-    
     int ConnectFD = accept(SocketFD, NULL, NULL);
 
     if(0 > ConnectFD)
@@ -113,8 +349,10 @@ int main(void)
       close(SocketFD);
       exit(EXIT_FAILURE);
     }
-    clients.push_back(ConnectFD);
+    //clients.push_back(ConnectFD);
+
     thread (listenClient, ConnectFD).detach();
+
     
   }
 
